b9b27d03d4248174bed38d3f98941a4dbd925fac
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-22 19:42:22 +0200

    Solution to the task

--- First Steps in OOP - Exercise/07.Programmer.py
+++ First Steps in OOP - Exercise/07.Programmer.py
@@ -6,13 +6,16 @@ class Programmer:
         self.skills = skills
 
     def watch_course(self, course_name, language, skills_earned):
+
         if self.language == language:
             self.skills += skills_earned
             return f"{self.name} watched {course_name}"
+
         return f"{self.name} does not know {language}"
 
     def change_language(self, new_language, skills_needed):
         if self.skills >= skills_needed:
+
             if self.language != new_language:
                 prev_language = self.language
                 self.language = new_language
