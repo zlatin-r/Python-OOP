cc39143a45ebb41ea4556f2c23ec3c9c30a74dda
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-23 12:47:21 +0200

    Solution to the task

---
+++ Classes and Objects - Lab/03.circle.py
@@ -0,0 +1,21 @@
+class Circle:
+
+    pi = 3.14
+
+    def __init__(self, radius: int):
+        self.radius = radius
+
+    def set_radius(self, new_radius: int):
+        self.radius = new_radius
+
+    def get_area(self):
+        return (self.radius ** 2) * self.pi
+
+    def get_circumference(self):
+        return (self.pi * 2) * self.radius
+
+
+circle = Circle(10)
+circle.set_radius(12)
+print(circle.get_area())
+print(circle.get_circumference())
