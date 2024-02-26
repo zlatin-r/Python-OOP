ae1efe4b16f367214db3d0c04867ebe87f47370f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-26 17:27:20 +0200

    Solution to the task

--- Classes and Objects - Exercise/04.pizza_delivery.py
+++ Classes and Objects - Exercise/04.pizza_delivery.py
@@ -3,7 +3,7 @@ class PizzaDelivery:
     def __init__(self, name, price, ingredients):
         self.name = name
         self.price = price
-        self.ingredients: ingredients = {}
+        self.ingredients = ingredients
         self.ordered = False
 
     def add_extra(self, ingredient, quantity, price_per_quantity):
@@ -25,8 +25,12 @@ class PizzaDelivery:
     def make_order(self):
         if not self.ordered:
             self.ordered = True
-            return f"You've ordered pizza {self.name} prepared with {self.ingredients} and the price will be {self.price}lv."
+            ingredients = ""
+            for k, v in self.ingredients.items():
+                ingredients += f"{k} : {v}, "
+            return f"You've ordered pizza {self.name} prepared with {ingredients}and the price will be {self.price}lv."
         return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
 
 margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
 margarita.add_extra('mozzarella', 1, 0.5)
@@ -37,4 +41,3 @@ print(margarita.remove_ingredient('tomatoes', 2, 0.5))
 margarita.remove_ingredient('cheese', 2, 1)
 print(margarita.make_order())
 print(margarita.add_extra('cheese', 1, 1))
-
