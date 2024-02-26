0e55f2d8d070f19d2ed3933869cc85ac583e947a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-26 18:59:48 +0200

    Solution to the task

--- Classes and Objects - Exercise/02.time.py
+++ Classes and Objects - Exercise/02.time.py
@@ -1,31 +1,34 @@
 class Time:
 
-    max_hour = 23
-    max_minute = 59
+    max_hours = 23
+    max_minutes = 59
     max_seconds = 59
 
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
-    def set_time(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
     def get_time(self):
-        return f"{self.hour}:{self.minute}:{self}"
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
     def next_second(self):
-        if self.second == Time.max_seconds:
-            self.second = 00
-            if self.minute == Time.max_minute:
-                self.minute += 00
-                if self.hour == Time.max_hour:
-                    self.hour = 00
-        else:
-            self.second += 1
+        self.seconds += 1
+
+        if self.seconds > Time.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes > Time.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours > Time.max_hours:
+                    self.hours = 0
+
         return self.get_time()
 
 
