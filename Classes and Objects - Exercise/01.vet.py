7650844ae7efe5108c703438e31bd66853264c8b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-26 16:27:31 +0200

    Solution to the task

--- Classes and Objects - Exercise/01.vet.py
+++ Classes and Objects - Exercise/01.vet.py
@@ -2,9 +2,9 @@ class Vet:
     animals = []
     space = 5
 
-    def __init__(self, name: str, animals: list):
+    def __init__(self, name: str):
         self.name = name
-        self.animals = animals
+        self.animals = []
 
     def register_animal(self, animal_name: str):
         if Vet.space > 0:
@@ -23,4 +23,19 @@ class Vet:
         return f"{animal_name} not in the clinic"
 
     def info(self):
-        return f"{self.name} has {len(Vet.animals)}. {Vet.space} space left in clinic"
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
