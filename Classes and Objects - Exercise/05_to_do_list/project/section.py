d7a29fd8081e7c14184966f0b4a39c46868ab4a5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-27 17:30:09 +0200

    Solution to the task

--- Classes and Objects - Exercise/05_to_do_list/project/section.py
+++ Classes and Objects - Exercise/05_to_do_list/project/section.py
@@ -20,7 +20,7 @@ class Section:
         try:
             task = next(filter(lambda t: t.name == task_name, self.tasks))
         except StopIteration:
-            return f"Could not find task with name {task_name}"
+            return f"Could not find task with the name {task_name}"
 
         task.completed = True
 
@@ -39,17 +39,3 @@ class Section:
     def view_section(self):
         tasks_with_details = "\n".join(t.details() for t in self.tasks)
         return f"Section {self.name}:\n{tasks_with_details}"
-
-
-task = Task("Make bed", "27/05/2020")
-print(task.change_name("Go to University"))
-print(task.change_due_date("28.05.2020"))
-task.add_comment("Don't forget laptop")
-print(task.edit_comment(0, "Don't forget laptop and notebook"))
-print(task.details())
-section = Section("Daily tasks")
-print(section.add_task(task))
-second_task = Task("Make bed", "27/05/2020")
-section.add_task(second_task)
-print(section.clean_section())
-print(section.view_section())
