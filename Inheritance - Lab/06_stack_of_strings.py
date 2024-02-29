b4d1d31377893e9694f514cdb49308ab722b1235
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-29 15:40:28 +0200

    Solution to the task

---
+++ Inheritance - Lab/06_stack_of_strings.py
@@ -0,0 +1,21 @@
+class Stack:
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        sorted_data = sorted(self.data, reverse=True)
+        result = ", ".join(sorted_data)
+        return f"[{result}]"
