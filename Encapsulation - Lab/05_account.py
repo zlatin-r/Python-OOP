19f3a6283a3a115c5a56e85f30b3217a609472d2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-02 13:50:00 +0200

    Solution to the task

--- Encapsulation - Lab/05_account.py
+++ Encapsulation - Lab/05_account.py
@@ -8,12 +8,14 @@ class Account:
     def get_id(self, pin):
         if pin == self.__pin:
             return self.__id
+
         return "Wrong pin"
 
     def change_pin(self, old_pin, new_pin):
         if old_pin == self.__pin:
             self.__pin = new_pin
             return "Pin changed"
+
         return "Wrong pin"
 
 
