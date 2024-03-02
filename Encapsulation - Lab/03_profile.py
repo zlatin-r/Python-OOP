42edd98cd22ec1965720cb37218e70eaed4e9e3a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-02 13:32:41 +0200

    Solution to the task

---
+++ Encapsulation - Lab/03_profile.py
@@ -0,0 +1,39 @@
+class Profile:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if not 5 <= len(value) <= 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        is_len_valid = len(value) >= 8
+        is_one_upper = len([c for c in value if c.isupper()]) > 0
+        is_one_digit = len([c for c in value if c.isdigit()]) > 0
+
+        is_valid = is_len_valid and is_one_upper and is_one_digit
+
+        if not is_valid:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.__username}" and ' \
+               f'password: {"*" * len(self.__password)}'
+
+
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
