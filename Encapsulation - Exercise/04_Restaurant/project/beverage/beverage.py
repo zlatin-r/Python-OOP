f43d4b4f91cc13525c460e00646996337b008140
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-04 16:44:37 +0200

    Solution to the task

--- Encapsulation - Exercise/04_Restaurant/project/beverage/beverage.py
+++ Encapsulation - Exercise/04_Restaurant/project/beverage/beverage.py
@@ -3,10 +3,10 @@ from project.product import Product
 
 class Beverage(Product):
     
-    def __init__(self, name, price, milliliters: float):
+    def __init__(self, name: str, price: float, milliliters: float):
         super().__init__(name, price)
-        self._milliliters = milliliters
+        self.__milliliters = milliliters
        
     @property
     def milliliters(self):
-        return self._milliliters
+        return self.__milliliters
