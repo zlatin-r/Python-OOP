b1e8a2c57cf8ee9c32b1ab0c2556b1b1d2966244
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-06 09:26:28 +0200

    Solution to the task

--- Encapsulation - Exercise/01_Wild_Cat_Zoo/project/lion.py
+++ Encapsulation - Exercise/01_Wild_Cat_Zoo/project/lion.py
@@ -2,9 +2,8 @@ from project.animal import Animal
 
 
 class Lion(Animal):
-    MONEY_FOR_CARE = 50
 
     def __init__(self, name, gender, age):
-        super().__init__(name, gender, age, self.MONEY_FOR_CARE)
-        self.name = name
+        super().__init__(name, gender, age, 50)
+
 
