866e9a580ec8cb81de967b748be5afa549c17762
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-07 12:29:18 +0200

    Solution to the task

--- Static and Class Methods - Lab/03_Integer.py
+++ Static and Class Methods - Lab/03_Integer.py
@@ -29,3 +29,13 @@ class Integer:
             return "wrong type"
 
         return cls(int(value))
+
+
+first_num = Integer(10)
+print(first_num.value)
+
+second_num = Integer.from_roman("IV")
+print(second_num.value)
+
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
