350b437b4aadd8e243fb62fa7e097bd25d8f5c13
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-07 12:15:43 +0200

    Solution to the task

--- Static and Class Methods - Lab/02_Shop.py
+++ Static and Class Methods - Lab/02_Shop.py
@@ -16,3 +16,32 @@ class Shop:
 
         self.items[item_name] = self.items.get(item_name, 0) + 1
         return f"{item_name} added to the shop"
+
+    def remove_item(self, item_name: str, amount: int):
+        if item_name in self.items.keys():
+            if self.items[item_name] >= amount:
+                self.items[item_name] -= amount
+
+                if self.items[item_name] == 0:
+                    self.items.pop(item_name)
+
+                return f"{amount} {item_name} removed from the shop"
+
+        return f"Cannot remove {amount} {item_name}"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
+
+
+fresh_shop = Shop("Fresh Shop", "Fruit and Veg", 50)
+small_shop = Shop.small_shop("Fashion Boutique", "Clothes")
+print(fresh_shop)
+print(small_shop)
+
+print(fresh_shop.add_item("Bananas"))
+print(fresh_shop.remove_item("Tomatoes", 2))
+
+print(small_shop.add_item("Jeans"))
+print(small_shop.add_item("Jeans"))
+print(small_shop.remove_item("Jeans", 2))
+print(small_shop.items)
