6d0338bfe9b85098f33db4e23a8b0a6d28b822fa
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-08 13:51:14 +0200

    Solution to the task

---
+++ Static and Class Methods - Exercise/02_Movie_World/project/dvd.py
@@ -0,0 +1,13 @@
+class DVD:
+
+    def __init__(self, name: str, _id: int, creation_year: int, creation_month: str, age_restriction: int )
+        self.name = name
+        self.id = _id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, _id: int, name: str, date: str, age_restriction: int):
+        return
