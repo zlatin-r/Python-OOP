14559751e8363d47ebcc0109a6e79cf4dfadb73c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-08 14:24:20 +0200

    Solution to the task

--- Static and Class Methods - Exercise/02_Movie_World/project/movie_world.py
+++ Static and Class Methods - Exercise/02_Movie_World/project/movie_world.py
@@ -39,3 +39,23 @@ class MovieWorld:
 
         customer.rented_dvds.append(dvd)
         dvd.is_rented = True
+
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id: int, dvd_id: int):
+        customer = next(filter(lambda c: c.id == customer_id, self.customers))
+        dvd = next(filter(lambda d: d.id == dvd_id, self.dvds))
+
+        if dvd not in customer.rented_dvds:
+            return f"{customer.name} does not have that DVD"
+
+        customer.rented_dvds.remove(dvd)
+        dvd.is_rented = False
+
+        return f"{customer.name} has successfully returned {dvd.name}"
+
+    def __repr__(self):
+        return "\n".join([
+            *[str(c) for c in self.customers],
+            *[str(d) for d in self.dvds],
+        ])
