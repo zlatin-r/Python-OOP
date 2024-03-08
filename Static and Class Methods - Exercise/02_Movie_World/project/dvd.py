11ab9b03c03263b6d1df8d451acf53be5424cf25
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-08 14:10:22 +0200

    Solution to the task

--- Static and Class Methods - Exercise/02_Movie_World/project/dvd.py
+++ Static and Class Methods - Exercise/02_Movie_World/project/dvd.py
@@ -1,6 +1,9 @@
+import calendar
+
+
 class DVD:
 
-    def __init__(self, name: str, _id: int, creation_year: int, creation_month: str, age_restriction: int ):
+    def __init__(self, name: str, _id: int, creation_year: int, creation_month: str, age_restriction: int):
         self.name = name
         self.id = _id
         self.creation_year = creation_year
@@ -10,4 +13,12 @@ class DVD:
 
     @classmethod
     def from_date(cls, _id: int, name: str, date: str, age_restriction: int):
-        return
+        day, month, year = [int(x) for x in date.split('.')]  # 23.12.2003
+        month_name = calendar.month_name[month]  # 12 => December
+
+        return cls(name, _id, year, month_name, age_restriction)
+
+    def __repr__(self):
+        return (f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) "
+                f"has age restriction {self.age_restriction}. "
+                f"Status: {'rented' if self.is_rented else 'not rented'}")
---
+++ Static and Class Methods - Exercise/02_Movie_World/project/movie_world.py
@@ -0,0 +1,41 @@
+from typing import List
+from project.customer import Customer
+from project.dvd import DVD
+
+
+class MovieWorld:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.customers: List[Customer] = []
+        self.dvds: List[DVD] = []
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = next(filter(lambda c: c.id == customer_id, self.customers))
+        dvd = next(filter(lambda d: d.id == dvd_id, self.dvds))
+
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+        if dvd.is_rented:
+            return f"DVD is already rented"
+        if customer.age < dvd.age_restriction:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
