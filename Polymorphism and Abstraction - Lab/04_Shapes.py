e3780584e645dda2a541d0d07962ebec50e06dd9
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-09 17:56:04 +0200

    Solution to the task

--- Polymorphism and Abstraction - Lab/04_Shapes.py
+++ Polymorphism and Abstraction - Lab/04_Shapes.py
@@ -15,26 +15,26 @@ class Shape(ABC):
 class Circle(Shape):
 
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def calculate_area(self):
-        return pi * (self.radius ** 2)
+        return pi * self.__radius ** 2
 
     def calculate_perimeter(self):
-        return 2 * pi * self.radius
+        return 2 * pi * self.__radius
 
 
 class Rectangle(Shape):
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def calculate_area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def calculate_perimeter(self):
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
 
 circle = Circle(5)
