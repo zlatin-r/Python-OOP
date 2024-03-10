ca4bdbb2583e1b606b8d07a428cc86e0be7d69e9
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-10 12:40:11 +0200

    Solution to the task

--- Polymorphism and Abstraction - Exercise/03_Account.py
+++ Polymorphism and Abstraction - Exercise/03_Account.py
@@ -5,4 +5,11 @@ class Account:
         self.amount = amount
         self._transactions = []
 
+    def handle_transaction(self, transaction_amount):
+        if transaction_amount > self.amount:
+            raise ValueError("sorry cannot go in debt!")
 
+        self.amount -= transaction_amount
+        self._transactions.append(transaction_amount)
+
+        return f"New balance: {self.amount}"
