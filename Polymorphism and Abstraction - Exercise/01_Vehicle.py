f3155930beb930c1ed32f5e0708424bad976a260
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-10 12:09:44 +0200

    Solution to the task

--- Polymorphism and Abstraction - Exercise/01_Vehicle.py
+++ Polymorphism and Abstraction - Exercise/01_Vehicle.py
@@ -20,7 +20,25 @@ class Car(Vehicle):
         self.fuel_increment = 0.9
 
     def drive(self, distance):
-        pass
+        fuel_needed = (self.fuel_consumption + self.fuel_increment) * distance
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
 
     def refuel(self, fuel):
-        pass
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+
+    def __init__(self, fuel_quantity: int, fuel_consumption: int):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+        self.fuel_increment = 1.6
+
+    def drive(self, distance):
+        fuel_needed = (self.fuel_consumption + self.fuel_increment) * distance
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
