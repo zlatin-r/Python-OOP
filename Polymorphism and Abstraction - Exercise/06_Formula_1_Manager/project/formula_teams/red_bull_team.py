96f0273365a7a0b958a8ce9c81ccd4f25aef7248
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-11 14:47:40 +0200

    Solution to the task

---
+++ Polymorphism and Abstraction - Exercise/06_Formula_1_Manager/project/formula_teams/red_bull_team.py
@@ -0,0 +1,15 @@
+from project.formula_teams.formula_team import FormulaTeam
+
+
+class RedBullTeam(FormulaTeam):
+    rewards = {1: 1500000, 2: 800000, 8: 20000, 10: 10000}
+    EXPENSES = 250000
+
+    def __init__(self, budget):
+        self.budget = budget
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        if race_pos in self.rewards:
+            revenue = self.rewards[race_pos] - self.EXPENSES
+            self.budget += revenue
+            return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
