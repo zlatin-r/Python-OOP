7b97ef26ead62c6635f2a11f43a533c6c99fae61
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-11 12:56:04 +0200

    Solution to the task

--- Polymorphism and Abstraction - Exercise/03_Account.py
+++ Polymorphism and Abstraction - Exercise/03_Account.py
@@ -1,32 +1,31 @@
 class Account:
 
-    def __init__(self, owner, amount=0):
+    def __init__(self, owner: str, amount: int = 0):
         self.owner = owner
         self.amount = amount
         self._transactions = []
 
     def handle_transaction(self, transaction_amount):
-        if transaction_amount > self.amount:
+        if self.balance + transaction_amount < 0:
             raise ValueError("sorry cannot go in debt!")
 
-        self.amount -= transaction_amount
         self._transactions.append(transaction_amount)
-
-        return f"New balance: {self.amount}"
+        return f"New balance: {self.balance}"
 
     def add_transaction(self, amount):
-        if not amount.isdigit():
+        if not isinstance(amount, int):
             raise ValueError("please use int for amount")
-        if self.amount < amount:
-            raise ValueError("sorry cannot go in debt!")
+        self.handle_transaction(amount)
 
-        self.amount -= amount
-        self._transactions.append(amount)
-
-        return f"New balance: {self.amount}"
-
+    @property
     def balance(self):
-        return sum(self._transactions) + self.amount
+        return self.amount + sum(self._transactions)
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
 
     def __len__(self):
         return len(self._transactions)
@@ -56,14 +55,32 @@ class Account:
         return self.balance >= obj.balance
 
     def __add__(self, obj):
-        concatenate_two_accounts = Account(f"{self.owner}&{obj.owner}", self.amount + obj.amount)
+        concatenate_two_accounts = Account(f"{self.owner}&{obj.owner}",  self.amount + obj.amount)
         concatenate_two_accounts._transactions = self._transactions + obj._transactions
         return concatenate_two_accounts
 
-    def __str__(self):
-        return f"Account of {self.owner} with starting amount: {self.amount}"
 
-    def __repr__(self):
-        return f"Account({self.owner}, {self.amount})"
-
-
+acc = Account('bob', 10)
+acc2 = Account('john')
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
