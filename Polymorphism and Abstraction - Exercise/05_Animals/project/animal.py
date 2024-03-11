bdfa1b730a06522befe8ec3e3d6542d72c84da27
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-11 14:12:42 +0200

    Solution to the task

--- Polymorphism and Abstraction - Exercise/05_Animals/project/animal.py
+++ Polymorphism and Abstraction - Exercise/05_Animals/project/animal.py
@@ -4,9 +4,9 @@ from abc import ABC, abstractmethod
 class Animal(ABC):
     def __init__(self, name, age, gender):
         self.name = name
-        self.gender = gender
         self.age = age
-    
+        self.gender = gender
+
     def __repr__(self):
         pass
 
