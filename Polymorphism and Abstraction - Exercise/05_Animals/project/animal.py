c04d5281a83d148909483ee2ecdae6398c0dd04d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-11 13:55:30 +0200

    Solution to the task

---
+++ Polymorphism and Abstraction - Exercise/05_Animals/project/animal.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def __repr__(self):
+        pass
+
+    def make_sound(self):
+        pass
