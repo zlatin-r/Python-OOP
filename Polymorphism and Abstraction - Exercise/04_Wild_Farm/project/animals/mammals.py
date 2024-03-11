717f618f98fdc47bfb087777dfad61bfea4e35da
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-11 13:32:58 +0200

    Solution to the task

--- Polymorphism and Abstraction - Exercise/04_Wild_Farm/project/animals/mammals.py
+++ Polymorphism and Abstraction - Exercise/04_Wild_Farm/project/animals/mammals.py
@@ -1,5 +1,33 @@
-from abc import ABC
+from project.animals.animal import Mammal
 
 
-class Mammals(ABC):
-    pass
+class Mouse(Mammal):
+    def __init__(self, name: str, weight: int, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Squeak"
+
+
+class Dog:
+    def __init__(self, name: str, weight: int, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Woof!"
+
+
+class Cat:
+    def __init__(self, name: str, weight: int, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "Meow"
+
+
+class Tiger:
+    def __init__(self, name: str, weight: int, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    def make_sound(self):
+        return "ROAR!!!"
