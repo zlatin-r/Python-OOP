d78b9e80f770be1d4dff5a932a366e651d1e2b73
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-17 11:03:38 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/02_09_12_2023/01-Structure-Skeleton/project/divers/free_diver.py
@@ -0,0 +1,19 @@
+from project.divers.base_diver import BaseDiver
+
+
+class FreeDiver(BaseDiver):
+    INITIAL_OXY_LEVEL = 123
+
+    def __init__(self, name):
+        super().__init__(name, self.INITIAL_OXY_LEVEL)
+
+    def miss(self, time_to_catch: int):
+        reduce_value = time_to_catch * 0.60
+        if self.oxygen_level < reduce_value:
+            self.oxygen_level = 0
+        else:
+            self.oxygen_level -= reduce_value
+        round(self.oxygen_level)
+
+    def renew_oxy(self):
+        self.oxygen_level = self.INITIAL_OXY_LEVEL
