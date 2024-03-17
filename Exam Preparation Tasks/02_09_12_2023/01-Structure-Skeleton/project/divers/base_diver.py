c61175ebc3f0e178dfd1d56f4ebffccd164a46b8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-17 10:42:36 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/02_09_12_2023/01-Structure-Skeleton/project/divers/base_diver.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class BaseDiver(ABC):
+    def __init__(self, name: str, oxygen_level: float):
+        self.name = name
+        self.oxygen_level = oxygen_level
+        self.catch = []
+        self.competition_points = 0.0
+        self.has_health_issue = False
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == "":
+            raise ValueError("Diver name cannot be null or empty!")
+        self.__name = value
+
+    @property
+    def oxygen_level(self):
+        return self.__oxygen_level
+
+    @oxygen_level.setter
+    def oxygen_level(self, value):
+        if value < 0:
+            raise ValueError("Cannot create diver with negative oxygen level!")
+        self.__oxygen_level = value
