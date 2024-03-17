74fa503674c9da61740bf07137c3d2bf70c7cef5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-17 10:55:01 +0200

    Solution to the task

--- Exam Preparation Tasks/02_09_12_2023/01-Structure-Skeleton/project/divers/base_diver.py
+++ Exam Preparation Tasks/02_09_12_2023/01-Structure-Skeleton/project/divers/base_diver.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from project.fish.base_fish import BaseFish
+
 
 class BaseDiver(ABC):
     def __init__(self, name: str, oxygen_level: float):
@@ -28,3 +30,27 @@ class BaseDiver(ABC):
         if value < 0:
             raise ValueError("Cannot create diver with negative oxygen level!")
         self.__oxygen_level = value
+
+    @abstractmethod
+    def miss(self, time_to_catch: int):
+        pass
+
+    @abstractmethod
+    def renew_oxy(self):
+        pass
+
+    def hit(self,fish: BaseFish):
+        if self.oxygen_level < fish.time_to_catch:
+            self.oxygen_level = 0
+        else:
+            self.oxygen_level -= fish.time_to_catch
+            self.competition_points += fish.points
+            self.catch.append(fish)
+
+    def update_health_status(self):
+        self.has_health_issue = True if self.has_health_issue else False
+
+    def __str__(self):
+        return (f"{self.__class__.__name__}: "
+                f"[Name: {self.name}, Oxygen level left: {self.oxygen_level}, "
+                f"Fish caught: {len(self.catch)}, Points earned: {self.competition_points}]")
