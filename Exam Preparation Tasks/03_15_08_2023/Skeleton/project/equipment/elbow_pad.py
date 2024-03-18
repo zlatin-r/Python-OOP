3e91147a5c6b60795694251b3f87bab1cb967149
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 17:31:57 +0200

    Solution to the task

--- Exam Preparation Tasks/03_15_08_2023/Skeleton/project/equipment/elbow_pad.py
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/equipment/elbow_pad.py
@@ -5,6 +5,7 @@ class ElbowPad(BaseEquipment):
     PROTECTION = 90
     PRICE = 25.0
     INCREASE_FACTOR = 0.10
+    TYPE_ = "ElbowPad"
 
     def __init__(self):
         super().__init__(self.PROTECTION, self.PRICE)
