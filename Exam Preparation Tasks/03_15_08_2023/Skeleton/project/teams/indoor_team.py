510dbbb172a3da92516a86b84317f4e2c99e37ce
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 17:32:02 +0200

    Solution to the task

--- Exam Preparation Tasks/03_15_08_2023/Skeleton/project/teams/indoor_team.py
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/teams/indoor_team.py
@@ -3,6 +3,7 @@ from project.teams.base_team import BaseTeam
 
 class IndoorTeam(BaseTeam):
     INITIAL_BUDGET = 500
+    TYPE_ = 'IndoorTeam'
 
     def __init__(self, name: str, country: str, advantage: int):
         super().__init__(name, country, advantage, self.INITIAL_BUDGET)
