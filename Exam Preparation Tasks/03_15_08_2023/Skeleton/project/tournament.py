340fee829fd8a653ad75f04a35bfd488e77590f4
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 18:25:43 +0200

    Solution to the task

--- Exam Preparation Tasks/03_15_08_2023/Skeleton/project/tournament.py
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/tournament.py
@@ -5,8 +5,8 @@ from project.teams.outdoor_team import OutdoorTeam
 
 
 class Tournament:
-    VALID_EQUIPMENT = {"KneePad": KneePad, "ElbowPad": ElbowPad}
-    VALID_TEAM_TYPES = {"OutdoorTeam": OutdoorTeam, "IndoorTeam": IndoorTeam}
+    EQUIPMENT_TYPES = {"KneePad": KneePad, "ElbowPad": ElbowPad}
+    TEAM_TYPES = {"OutdoorTeam": OutdoorTeam, "IndoorTeam": IndoorTeam}
 
     def __init__(self, name: str, capacity: int):
         self.name = name
@@ -25,82 +25,73 @@ class Tournament:
         self.__name = value
 
     def add_equipment(self, equipment_type: str):
-        if equipment_type not in self.VALID_EQUIPMENT:
-            return ValueError("Invalid equipment type!")
-        new_equipment = self.VALID_EQUIPMENT[equipment_type]
+        if equipment_type not in self.EQUIPMENT_TYPES:
+            raise ValueError("Invalid equipment type!")
+        new_equipment = self.EQUIPMENT_TYPES[equipment_type]()
         self.equipment.append(new_equipment)
         return f"{equipment_type} was successfully added."
 
     def add_team(self, team_type: str, team_name: str, country: str, advantage: int):
-        if team_type not in self.VALID_TEAM_TYPES:
+        if team_type not in self.TEAM_TYPES:
             raise ValueError("Invalid team type!")
-        if self.capacity <= 0:
+        if len(self.teams) >= self.capacity:
             return "Not enough tournament capacity."
-        new_team = self.VALID_TEAM_TYPES[team_type](team_name, country, advantage)
+        new_team = self.TEAM_TYPES[team_type](team_name, country, advantage)
         self.teams.append(new_team)
-        self.capacity -= 1
         return f"{team_type} was successfully added."
 
     def sell_equipment(self, equipment_type: str, team_name: str):
-        equipment = self._find_equipment(equipment_type)
-        team = self._find_team(team_name)
-
-        if team.budget < equipment.PRICE:
+        equipment = self._find_last_equipment_by_type(equipment_type)
+        team = self._find_team_by_name(team_name)
+        if team.budget < equipment.price:
             raise Exception("Budget is not enough!")
-        team.budget -= equipment.PRICE
-        team.equipment.append(equipment)
         self.equipment.remove(equipment)
+        team.equipment.append(equipment)
+        team.budget -= equipment.price
         return f"Successfully sold {equipment_type} to {team_name}."
 
     def remove_team(self, team_name: str):
-        team = self._find_team(team_name)
-        if not team:
+        team = self._find_team_by_name(team_name)
+        if team is None:
             raise Exception("No such team!")
-        if team.wins > 0:
+        if team.wins:
             raise Exception(f"The team has {team.wins} wins! Removal is impossible!")
         self.teams.remove(team)
-        self.capacity += 1
         return f"Successfully removed {team_name}."
 
     def increase_equipment_price(self, equipment_type: str):
-        changed_eq_pcs = len([eq.increase_price for eq in self.equipment if eq.TYPE_ == equipment_type])
+        changed_eq_pcs = len([eq.increase_price() for eq in self.equipment if eq.TYPE_ == equipment_type])
         return f"Successfully changed {changed_eq_pcs}pcs of equipment."
 
     def play(self, team_name1: str, team_name2: str):
-        winner = ""
-        team1 = self._find_team(team_name1)
-        team2 = self._find_team(team_name2)
-
-        if type(team1).__name__ != type(team2).__name__:
-            raise Exception("Game cannot start! Team types mismatch!")
-
-        team1_points = team1.advantage + sum(e.PROTECTION for e in team1.equipment)
-        team2_points = team2.advantage + sum(e.PROTECTION for e in team2.equipment)
-
-        if team1_points == team2_points:
-            return "No winner in this game."
-
+        team1 = self._find_team_by_name(team_name1)
+        team2 = self._find_team_by_name(team_name2)
+        if not team1.TYPE_ == team2.TYPE_:
+            raise Exception(f"Game cannot start! Team types mismatch!")
+        team1_points = team1.sum_points()
+        team2_points = team2.sum_points()
         if team1_points > team2_points:
             team1.win()
-            winner = team1.name
-        else:
+            return f"The winner is {team1.name}."
+        if team1_points < team2_points:
             team2.win()
-            winner = team2.name
-
-        return f"The winner is {winner}."
+            return f"The winner is {team2.name}."
+        return "No winner in this game."
 
     def get_statistics(self):
         sorted_teams = sorted(self.teams, key=lambda t: -t.wins)
-        result = [(f"Tournament: {self.name}\n"
-                   f"Number of Teams: {len(self.teams)}\n"
-                   "Teams:")]
+        result = [f"""Tournament: {self.name}
+Number of Teams: {len(self.teams)}
+Teams:"""]
         [result.append(t.get_statistics()) for t in sorted_teams]
         return '\n'.join(result)
 
-    def _find_equipment(self, equipment_type: str):
+    # helper methods
+
+    def _find_last_equipment_by_type(self, equipment_type):
         collection = [eq for eq in self.equipment if eq.TYPE_ == equipment_type]
         return collection[-1] if collection else None
 
-    def _find_team(self, team_name: str):
+    def _find_team_by_name(self, team_name):
         collection = [t for t in self.teams if t.name == team_name]
         return collection[0] if collection else None
