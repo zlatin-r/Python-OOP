115d3e61f576f39f051bc60994a45e98c8b04c6e
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 18:56:18 +0200

    Renamed and Deleted Files

---
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/teams/outdoor_team.py
@@ -0,0 +1,9 @@
+from project.teams.base_team import BaseTeam
+
+
+class OutdoorTeam(BaseTeam):
+    def __init__(self, name, country, advantage):
+        super().__init__(name, country, advantage, budget=1000)
+
+    def win(self):
+        self.advantage += 115
