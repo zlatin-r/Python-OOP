8fcc725e504a4046a2e4231127d4f8bcd7cc63a8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 17:51:24 +0200

    Solution to the task

--- Exam Preparation Tasks/03_15_08_2023/Skeleton/project/equipment/knee_pad.py
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/equipment/knee_pad.py
@@ -4,11 +4,10 @@ from project.equipment.base_equipment import BaseEquipment
 class KneePad(BaseEquipment):
     PROTECTION = 120
     PRICE = 15.0
-    INCREASE_FACTOR = 0.20
     TYPE_ = "KneePad"
 
     def __init__(self):
-        super().__init__(protection=self.PROTECTION, price=self.PRICE)
+        super().__init__(self.PROTECTION, self.PRICE)
 
     def increase_price(self):
-        self.PRICE += self.PRICE * self.INCREASE_FACTOR
+        self.PRICE *= 1.2
