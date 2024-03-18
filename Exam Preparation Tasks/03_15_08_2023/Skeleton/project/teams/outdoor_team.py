aac3d533d5b9b90329ab4889c629d092b5c0581f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 18:25:40 +0200

    Solution to the task

--- Exam Preparation Tasks/03_15_08_2023/Skeleton/project/teams/outdoor_team.py
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/teams/outdoor_team.py
@@ -2,12 +2,13 @@ from project.teams.base_team import BaseTeam
 
 
 class OutdoorTeam(BaseTeam):
-    INITIAL_BUDGET = 1000
+    BUDGET = 1000.0
+    INCREMENT_ADVANTAGE = 115
     TYPE_ = 'OutdoorTeam'
 
     def __init__(self, name: str, country: str, advantage: int):
-        super().__init__(name, country, advantage, budget=self.INITIAL_BUDGET)
+        super().__init__(name, country, advantage, budget=self.BUDGET)
 
     def win(self):
-        self.advantage += 115
+        self.advantage += self.INCREMENT_ADVANTAGE
         self.wins += 1
