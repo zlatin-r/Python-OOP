c0a9c6eef8065313db35df6cfcbfe885f638eddf
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-18 18:37:47 +0200

    Renamed and Deleted Files

---
+++ Exam Preparation Tasks/03_15_08_2023/Skeleton/project/equipment/knee_pad.py
@@ -0,0 +1,9 @@
+from project.equipment.base_equipment import BaseEquipment
+
+
+class KneePad(BaseEquipment):
+    def __init__(self):
+        super().__init__(protection=120, price=15)
+
+    def increase_price(self):
+        self.price *= 1.20
