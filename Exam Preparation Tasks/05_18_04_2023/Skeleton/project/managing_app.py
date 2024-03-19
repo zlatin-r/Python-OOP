482d7450cf4323f14c5b0d1f9d1c9d9a22c29d7b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-19 16:08:11 +0200

    Solution to the task

--- Exam Preparation Tasks/05_18_04_2023/Skeleton/project/managing_app.py
+++ Exam Preparation Tasks/05_18_04_2023/Skeleton/project/managing_app.py
@@ -5,7 +5,8 @@ from project.vehicles.passenger_car import PassengerCar
 
 
 class ManagingApp:
-    VALID_VEHICLES = {"PassengerCar":PassengerCar,"CargoVan": CargoVan}
+    VALID_VEHICLES = {"PassengerCar": PassengerCar, "CargoVan": CargoVan}
+
     def __init__(self):
         self.users = []
         self.vehicles = []
@@ -13,7 +14,7 @@ class ManagingApp:
 
     def register_user(self, first_name: str, last_name: str, driving_license_number: str):
         user = next(filter(lambda u: u.driving_license_number == driving_license_number, self.users), None)
-        if not user:
+        if user:
             return f"{driving_license_number} has already been registered to our platform."
         new_user = User(first_name, last_name, driving_license_number)
         self.users.append(new_user)
@@ -41,8 +42,10 @@ class ManagingApp:
                     route.is_locked = True
         new_road = Route(start_point, end_point, length, route_id)
         self.routes.append(new_road)
+        return f"{start_point}/{end_point} - {length} km is unlocked and available to use."
 
-    def make_trip(self, driving_license_number: str, license_plate_number: str, route_id: int,  is_accident_happened: bool):
+    def make_trip(self, driving_license_number: str, license_plate_number: str, route_id: int,
+                  is_accident_happened: bool):
         user = next(filter(lambda u: u.driving_license_number == driving_license_number, self.users))
         vehicle = next(filter(lambda v: v.license_plate_number == license_plate_number, self.vehicles))
         route = next(filter(lambda r: r.route_id == route_id, self.routes))
@@ -56,16 +59,21 @@ class ManagingApp:
         if is_accident_happened:
             vehicle.is_damaged = True
             user.decrease_rating()
-        user.increase_rating()
+        else:
+            user.increase_rating()
         return vehicle
 
     def repair_vehicles(self, count: int):
-        damaged_vehicles = [v.recharge().change_status() for v in self.vehicles if v.is_damaged]
+        damaged_vehicles = [v for v in self.vehicles if v.is_damaged]
+        for v in damaged_vehicles:
+            v.is_damaged = False
+            v.battery_level = 100
         sorted_vehicles = sorted(damaged_vehicles, key=lambda v: (v.brand, v.model))
         result = sorted_vehicles[:count] if count > len(sorted_vehicles) else sorted_vehicles
         return f"{len(result)} vehicles were successfully repaired!"
 
     def users_report(self):
         sorted_users = sorted(self.users, key=lambda u: -u.rating)
-        result = "*** E-Drive-Rent ***\n"
-        result += "\n".join(str(u) for u in  sorted_users)
+        result = ["*** E-Drive-Rent ***"]
+        result.append("\n".join(str(u) for u in sorted_users))
+        return '\n'.join(result)
