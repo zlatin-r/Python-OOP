9f0470de0a2294f338e81668c073e45123c3e462
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-19 09:50:24 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/04_05_08_2023/Structure-Skeleton/project/clients/adult.py
@@ -0,0 +1,11 @@
+from project.clients.base_client import BaseClient
+
+
+class Adult(BaseClient):
+    INITIAL_INTEREST = 4
+
+    def __init__(self, name, client_id, income):
+        super().__init__(name, client_id, income, interest=self.INITIAL_INTEREST)
+
+    def increase_clients_interest(self):
+        self.interest += 2
---
+++ Exam Preparation Tasks/04_05_08_2023/Structure-Skeleton/project/clients/student.py
@@ -0,0 +1,11 @@
+from project.clients.base_client import BaseClient
+
+
+class Student(BaseClient):
+    INITIAL_INTEREST = 2
+
+    def __init__(self, name, client_id, income):
+        super().__init__(name, client_id, income, interest=self.INITIAL_INTEREST)
+
+    def increase_clients_interest(self):
+        self.interest += 1
