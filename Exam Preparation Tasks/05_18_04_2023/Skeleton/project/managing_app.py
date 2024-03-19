e71a74e88772ab4ccf0c130b972aabfb5491aa74
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-19 14:44:38 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/05_18_04_2023/Skeleton/project/managing_app.py
@@ -0,0 +1,38 @@
+from project.user import User
+from project.vehicles.cargo_van import CargoVan
+from project.vehicles.passenger_car import PassengerCar
+
+
+class ManagingApp:
+    VALID_VEHICLES = {"PassengerCar":PassengerCar,"CargoVan": CargoVan}
+    def __init__(self):
+        self.users = []
+        self.vehicles = []
+        self.routes = []
+
+    def register_user(self, first_name: str, last_name: str, driving_license_number: str):
+        user = next(filter(lambda u: u.driving_license_number == driving_license_number, self.users), None)
+        if not user:
+            return f"{driving_license_number} has already been registered to our platform."
+        new_user = User(first_name, last_name, driving_license_number)
+        self.users.append(new_user)
+        return f"{first_name} {last_name} was successfully registered under DLN-{driving_license_number}"
+
+    def upload_vehicle(self, vehicle_type: str, brand: str, model: str, license_plate_number: str):
+        if vehicle_type not in self.VALID_VEHICLES:
+            return f"Vehicle type {vehicle_type} is inaccessible."
+        vehicle = next(filter(lambda v: v.license_plate_number == license_plate_number, self.vehicles), None)
+        if vehicle:
+            return f"{license_plate_number} belongs to another vehicle."
+        new_vehicle = self.VALID_VEHICLES[vehicle_type](brand, model, license_plate_number)
+        self.vehicles.append(new_vehicle)
+        return f"{brand} {model} was successfully uploaded with LPN-{license_plate_number}."
+
+    def allow_route(self, start_point: str, end_point: str, length: float):
+        route_id = len(self.routes) + 1
+        for route in self.routes:
+            if route.start_point == start_point and route.end_point == end_point and route.length == length:
+                return f"{start_point}/{end_point} - {length} km had already been added to our platform."
+            if route.start_point == start_point and route.end_point == end_point and route.length < length:
+                return f"{start_point}/{end_point} shorter route had already been added to our platform."
+
