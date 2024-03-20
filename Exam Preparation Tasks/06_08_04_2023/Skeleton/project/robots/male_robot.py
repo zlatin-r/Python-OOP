e88c67f1a1138f25656d008556533d6607889f0f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-20 19:14:00 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/06_08_04_2023/Skeleton/project/robots/male_robot.py
@@ -0,0 +1,12 @@
+from project.robots.base_robot import BaseRobot
+
+
+class MaleRobot(BaseRobot):
+    INITIAL_WEIGHT = 9
+    SERVICE = "MainService"
+
+    def __init__(self, name: str, kind: str, price: float):
+        super().__init__(name, kind, price, weight=self.INITIAL_WEIGHT)
+
+    def eating(self):
+        self.weight += 3
