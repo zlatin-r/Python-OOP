8fe71d1de1fe5eb28db6d458775f0b2b11c9df77
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-20 19:14:15 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/06_08_04_2023/Author's Solution/project/services/secondary_service.py
@@ -0,0 +1,12 @@
+from project.services.base_service import BaseService
+
+
+class SecondaryService(BaseService):
+    CAPACITY = 15
+
+    def __init__(self, name: str):
+        super().__init__(name, capacity=self.CAPACITY)
+
+    def details(self):
+        return f"""{self.name} Secondary Service:
+Robots: {self._get_names()}"""
