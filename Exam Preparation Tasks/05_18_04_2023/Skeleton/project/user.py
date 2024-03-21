67eb8beb6c617809a0c69cd4d5e2db8fa7aea6ad
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-21 09:46:46 +0200

    Solution to the task

--- Exam Preparation Tasks/05_18_04_2023/Skeleton/project/user.py
+++ Exam Preparation Tasks/05_18_04_2023/Skeleton/project/user.py
@@ -1,9 +1,14 @@
 class User:
+    MAX_RATING = 10
+    INCREASE_STEP = 0.5
+    MIN_RATING = 0
+    DECREASE_STEP = 2.0
+
     def __init__(self, first_name: str, last_name: str, driving_license_number: str):
         self.first_name = first_name
         self.last_name = last_name
         self.driving_license_number = driving_license_number
-        self.rating = 0
+        self.rating = self.MIN_RATING
         self.is_blocked = False
 
     @property
@@ -42,22 +47,21 @@ class User:
 
     @rating.setter
     def rating(self, value):
-        if value < 0:
+        if value < self.MIN_RATING:
             raise ValueError("Users rating cannot be negative!")
         self.__rating = value
 
     def increase_rating(self):
-        self.rating += 0.5
-        if self.rating > 10:
-            self.rating = 10
+        self.rating += self.INCREASE_STEP
+        if self.rating > self.MAX_RATING:
+            self.rating = self.MAX_RATING
 
     def decrease_rating(self):
-        if self.rating - 2 < 0:
-            self.rating = 0
+        if self.rating - self.DECREASE_STEP < self.MIN_RATING:
+            self.rating = self.MIN_RATING
             self.is_blocked = True
         else:
-            self.rating -= 2
+            self.rating -= self.DECREASE_STEP
 
     def __str__(self):
-        return (f"{self.first_name} {self.last_name} "
-                f"Driving license: {self.driving_license_number} Rating: {self.rating}")
+        return f"{self.first_name} {self.last_name} Driving license: {self.driving_license_number} Rating: {self.rating}"
