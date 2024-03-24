1f5590c11f062165c654d485ddcaf7fb7f9c2dfa
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-24 14:12:55 +0200

    Solution to the task

--- Testing - Lab/Lab-Resources/Test Cat/test_cat.py
+++ Testing - Lab/Lab-Resources/Test Cat/test_cat.py
@@ -7,16 +7,20 @@ class TestCat(TestCase):
         self.cat = Cat("Tom")
 
     def test_correct_init(self):
-        self.assertEqual(self.cat.name, "Tom")
+        self.assertEqual("Tom", self.cat.name)
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
 
-    def test_eat_if_is_fed_expected_exception(self):
-        self.cat.fed = True
-
-        with self.assertRaises(Exception) as ex:
-            self.cat.eat()
-
-        self.assertEqual(str(ex.exception), "Already fed.")
-
+    # def test_eat_if_is_fed_expected_exception(self):
+    #     self.cat.fed = True
+    #
+    #     with self.assertRaises(Exception) as ex:
+    #         self.cat.eat()
+    #
+    #     self.assertEqual(str(ex.exception), "Already fed.")
+    #
+    # def test_eat_if_is_not_fed(self):
 
 if __name__ == '__main__':
     main()
