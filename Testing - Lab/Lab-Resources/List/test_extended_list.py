4b01f4ba840bab45e1ba54ecb5ca08334f20e819
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-24 14:39:18 +0200

    Solution to the task

--- Testing - Lab/Lab-Resources/List/test_extended_list.py
+++ Testing - Lab/Lab-Resources/List/test_extended_list.py
@@ -10,6 +10,12 @@ class TestExtendedList(TestCase):
     def test_correct_init_ignores_non_integer_values(self):
         self.assertEqual([1, 2, 3], self.i_list.get_data())
 
+    def test_add_non_integer_elements_to_the_list_raises_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.i_list.add(5.5)
+
+        self.assertEqual("Element is not Integer", str(ve.exception))
+
 
 if __name__ == '__main__':
     main()
