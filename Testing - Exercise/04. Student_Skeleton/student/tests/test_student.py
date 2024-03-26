6749de7967fdc31ad7197ed0590d6cf55c82080c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-26 13:47:36 +0200

    Solution to the task

--- Testing - Exercise/04. Student_Skeleton/student/tests/test_student.py
+++ Testing - Exercise/04. Student_Skeleton/student/tests/test_student.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, main
+
 from project.student import Student
 
 
@@ -13,6 +14,18 @@ class TestStudent(TestCase):
 
         self.assertEqual({}, self.student.courses)
         self.assertEqual({"math": ["x + y = z"]}, self.student_with_courses.courses)
+
+    def test_enroll_in_the_same_course_appends_new_notes(self):
+        result = self.student_with_courses.enroll(
+            "math", ["1 + 2 = 3", "3 + 4 = 7"])
+
+        self.assertEqual("Course already added. Notes have been updated.", result)
+
+        self.assertEqual(
+            ["x + y = z", "1 + 2 = 3", "3 + 4 = 7"],
+            self.student_with_courses.courses["math"])
+
+    def
 
 
 if __name__ == '__main__':
