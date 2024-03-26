7223a9a7f617a482dcdb77ccb0fd693bb778ae62
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-26 13:40:51 +0200

    Solution to the task

--- Testing - Exercise/04. Student_Skeleton/student/tests/test_student.py
+++ Testing - Exercise/04. Student_Skeleton/student/tests/test_student.py
@@ -8,8 +8,11 @@ class TestStudent(TestCase):
         self.student_with_courses = Student("Test2", {"math": ["x + y = z"]})
 
     def test_init(self):
-        self.assertEqual("Test1", self.student)
-        self.assertEqual("Test2", self.student_with_courses)
+        self.assertEqual("Test1", self.student.name)
+        self.assertEqual("Test2", self.student_with_courses.name)
+
+        self.assertEqual({}, self.student.courses)
+        self.assertEqual({"math": ["x + y = z"]}, self.student_with_courses.courses)
 
 
 if __name__ == '__main__':
