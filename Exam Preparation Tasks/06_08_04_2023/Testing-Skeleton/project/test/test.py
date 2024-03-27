70a937ddd887ff0506f38ef7f785089521ccb6e5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-27 15:45:33 +0200

    Solution to the task

--- Exam Preparation Tasks/06_08_04_2023/Testing-Skeleton/project/test/test.py
+++ Exam Preparation Tasks/06_08_04_2023/Testing-Skeleton/project/test/test.py
@@ -1,1 +1,17 @@
+from unittest import TestCase, main
 from project.tennis_player import TennisPlayer
+
+
+class TestTennisPlayer(TestCase):
+    def setUp(self):
+        self.tennis_player = TennisPlayer("Ivan", 30, 10.0)
+
+    def test_correct_init(self):
+        self.assertEqual("Ivan", self.tennis_player.name)
+        self.assertEqual(30, self.tennis_player.age)
+        self.assertEqual(10.0, self.tennis_player.points)
+        self.assertEqual([], self.tennis_player.wins)
+
+
+if __name__ == '__main__':
+    main()
