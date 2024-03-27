35d27af1a4cc16d1417dd1aae533c59f34a76675
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-27 15:48:58 +0200

    Solution to the task

--- Exam Preparation Tasks/06_08_04_2023/Testing-Skeleton/project/test/test.py
+++ Exam Preparation Tasks/06_08_04_2023/Testing-Skeleton/project/test/test.py
@@ -12,6 +12,18 @@ class TestTennisPlayer(TestCase):
         self.assertEqual(10.0, self.tennis_player.points)
         self.assertEqual([], self.tennis_player.wins)
 
+    def test_setter_name_with_len_two_raises_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.tennis_player.name = "Jo"
+
+        self.assertEqual("Name should be more than 2 symbols!", str(ve.exception))
+
+    def test_setter_age_value_less_than_18_raises_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.tennis_player.age = 17
+
+        self.assertEqual("Players must be at least 18 years of age!", str(ve.exception))
+
 
 if __name__ == '__main__':
     main()
