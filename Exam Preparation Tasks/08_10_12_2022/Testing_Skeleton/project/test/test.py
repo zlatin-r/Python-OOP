44e532bb8bb285c989d8808a11260a3ba28e1eef
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-28 09:57:12 +0200

    Solution to the task

--- Exam Preparation Tasks/08_10_12_2022/Testing_Skeleton/project/test/test.py
+++ Exam Preparation Tasks/08_10_12_2022/Testing_Skeleton/project/test/test.py
@@ -1,1 +1,22 @@
+from unittest import TestCase, main
 from project.toy_store import ToyStore
+
+
+class TestToyStore(TestCase):
+    def setUp(self) -> None:
+        self.toy_store = ToyStore()
+
+    def test_correct_init(self):
+        self.assertEqual(self.toy_store.toy_shelf, {
+            "A": None,
+            "B": None,
+            "C": None,
+            "D": None,
+            "E": None,
+            "F": None,
+            "G": None,
+        })
+
+
+if __name__ == '__main__':
+    main()
