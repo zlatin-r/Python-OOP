f8f61bfa620b73bb1ec04af513e091e9dd368e45
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-28 10:29:06 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/09_22_08_2022/01. Structure_Skeleton/project/client.py
@@ -0,0 +1,16 @@
+class Client:
+    def __init__(self, phone_number: str):
+        self.phone_number = phone_number
+        self.shopping_cart = []
+        self.bill = 0.0
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        if value[0] != 0 or len(value) != 10 or not value.isnumeric():
+            raise ValueError("Invalid phone number!")
+        self.__phone_number = value
+
