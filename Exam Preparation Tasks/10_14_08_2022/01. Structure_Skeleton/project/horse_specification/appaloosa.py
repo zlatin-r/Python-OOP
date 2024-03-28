567f97abb0aa98d5ccf7d0dc7bd4a0d2ede5e54e
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-28 13:48:15 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/10_14_08_2022/01. Structure_Skeleton/project/horse_specification/appaloosa.py
@@ -0,0 +1,11 @@
+from project.horse_specification.horse import Horse
+
+
+class Appaloosa(Horse):
+    MAX_SPEED = 120
+
+    def __init__(self, name, speed):
+        super().__init__(name, speed)
+
+    def train(self):
+        self.speed += 2
