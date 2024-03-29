818ec1fad44ace944b8f6e1d154a434e9cc4ed11
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-29 16:56:45 +0200

    Solution to the task

--- Exam Preparation Tasks/11_18_04_2022/03. Unit Testing_Skeleton/project/test/test_plantation.py
+++ Exam Preparation Tasks/11_18_04_2022/03. Unit Testing_Skeleton/project/test/test_plantation.py
@@ -11,6 +11,27 @@ class TestPlantation(TestCase):
         self.assertEqual({}, self.plantation.plants)
         self.assertEqual([], self.plantation.workers)
 
+    def test_size_setter_invalid_value_raises_exception(self):
+        with self.assertRaises(ValueError) as ve:
+            self.plantation.size = -1
+
+        self.assertEqual("Size must be positive number!", str(ve.exception))
+
+    def test_hire_worker_but_already_hired_raise_exception(self):
+        self.plantation.workers = ["Worker"]
+
+        with self.assertRaises(ValueError) as ve:
+            self.plantation.hire_worker("Worker")
+
+        self.assertEqual("Worker already hired!", str(ve.exception))
+
+    def test_hire_worker_happy_case(self):
+        res = self.plantation.hire_worker("Worker")
+
+        self.assertEqual("Worker successfully hired.", res)
+        self.assertEqual(["Worker"], self.plantation.workers)
+
+    def tesr
 
 if __name__ == '__main__':
     main()
