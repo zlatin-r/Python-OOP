8453e250ef4e50bab7c02afe891f319e8def870b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-29 12:59:11 +0200

    Solution to the task

--- Exam Preparation Tasks/11_18_04_2022/01. Structure_Skeleton/project/movie_app.py
+++ Exam Preparation Tasks/11_18_04_2022/01. Structure_Skeleton/project/movie_app.py
@@ -56,13 +56,44 @@ class MovieApp:
         if movie in user.movies_owned:
             raise Exception(f"{username} is the owner of the movie {movie.title}!")
 
-        if movie in user.movies_liked:
+        if self._check_if_user_likes_the_movie(user, movie):
             raise Exception(f"{username} already liked the movie {movie.title}!")
 
         movie.likes += 1
         user.movies_liked.append(movie)
         return f"{username} liked {movie.title} movie."
 
+    def dislike_movie(self, username: str, movie: Movie):
+        user = self._find_user_by_username(username)
+
+        if not self._check_if_user_likes_the_movie(user, movie):
+            raise Exception(f"{username} has not liked the movie {movie.title}!")
+
+        movie.likes -= 1
+        user.movies_liked.remove(movie)
+        return f"{username} disliked {movie.title} movie."
+
+    def display_movies(self):
+        if not self.movies_collection:
+            return "No movies found."
+
+        sorted_movies = sorted(self.movies_collection, key=lambda movie: (-movie.year, movie.title))
+        result = [m.details() for m in sorted_movies]
+        return "\n".join(result)
+
+    def __str__(self):
+        result = []
+        if not self.users_collection:
+            result.append("All users: No users.")
+        else:
+            users = [u.username for u in self.users_collection]
+            result.append(f"All users:{', '.join(users)}")
+
+        if not self.movies_collection:
+            result.append("All movies: No movies.")
+        else:
+            movies = [m.title for m in self.movies_collection]
+            result.append(f"All movies:{', '.join(movies)}")
 
 
     # HELPING METHODS:
@@ -78,3 +109,6 @@ class MovieApp:
     def _check_if_movie_is_uploaded(self, movie: Movie):
         if movie not in self.movies_collection:
             raise Exception("The movie {movie_title} is not uploaded!")
+
+    def _check_if_user_likes_the_movie(self, user: User, movie: Movie):
+        return movie in user.movies_liked
