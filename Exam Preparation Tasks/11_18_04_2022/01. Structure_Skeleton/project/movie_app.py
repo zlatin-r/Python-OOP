5efce0e5e2bffc53cc7803de423ba30045b47de6
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-29 12:20:14 +0200

    Solution to the task

---
+++ Exam Preparation Tasks/11_18_04_2022/01. Structure_Skeleton/project/movie_app.py
@@ -0,0 +1,40 @@
+from project.movie_specification.movie import Movie
+from project.user import User
+
+
+class MovieApp:
+    def __init__(self):
+        self.movies_collection = []
+        self.users_collection = []
+
+    def register_user(self, username: str, age: int):
+        if self._find_user_by_username(username):
+            raise Exception("User already exists!")
+
+        new_user = User(username, age)
+        self.users_collection.append(new_user)
+        return f"{username} registered successfully."
+
+    def upload_movie(self, username: str, movie: Movie):
+        user = self._find_user_by_username(username)
+
+        if user:
+            raise Exception("This user does not exist!")
+
+        if movie not in user.movies_owned:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
+
+        if movie in self.movies_collection:
+            raise Exception("Movie already added to the collection!")
+
+        user.movies_owned.append(movie)
+        self.movies_collection.append(movie)
+        return f"{username} successfully added {movie.title} movie."
+
+
+
+    # HELPING METHODS:
+
+    def _find_user_by_username(self, username: str):
+        user = next(filter(lambda u: u.username == username, self.users_collection), None)
+        return user
