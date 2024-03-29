12433d00f27995e59a5bc0c93b4360a0bf2a6741
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-29 16:50:34 +0200

    Solution to the task

--- Exam Preparation Tasks/11_18_04_2022/03. Unit Testing_Skeleton/project/test/test_plantation.py
+++ Exam Preparation Tasks/11_18_04_2022/03. Unit Testing_Skeleton/project/test/test_plantation.py
@@ -1,1 +1,16 @@
+from unittest import TestCase, main
 from project.plantation import Plantation
+
+
+class TestPlantation(TestCase):
+    def setUp(self):
+        self.plantation = Plantation(10)
+
+    def test_correct_init(self):
+        self.assertEqual(10, self.plantation.size)
+        self.assertEqual({}, self.plantation.plants)
+        self.assertEqual([], self.plantation.workers)
+
+
+if __name__ == '__main__':
+    main()
