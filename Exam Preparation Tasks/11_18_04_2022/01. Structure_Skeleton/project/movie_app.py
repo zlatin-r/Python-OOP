b06da89dfd903d723952dd87a5ba2320e2e452d5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-29 12:24:27 +0200

    Solution to the task

--- Exam Preparation Tasks/11_18_04_2022/01. Structure_Skeleton/project/movie_app.py
+++ Exam Preparation Tasks/11_18_04_2022/01. Structure_Skeleton/project/movie_app.py
@@ -31,6 +31,15 @@ class MovieApp:
         self.movies_collection.append(movie)
         return f"{username} successfully added {movie.title} movie."
 
+    def edit_movie(self, username: str, movie: Movie, **kwargs):
+        user = self._find_user_by_username(username)
+
+        if movie not in user.movies_owned:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
+
+        if movie not in self.movies_collection:
+            raise Exception(f"The movie {movie.title} is not uploaded!"
+)
 
 
     # HELPING METHODS:
