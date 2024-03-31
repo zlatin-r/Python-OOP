74d77674b8f473e7269bc2a688a630a3652e1d18
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-31 12:34:18 +0300

    Solution to the task

---
+++ Decorators - Lab/04_Multiply.py
@@ -0,0 +1,15 @@
+def multiply(num):
+    def decorator(ref_func):
+        def wrapper(*args, **kwargs):
+            result = ref_func(*args, **kwargs)
+            return result * num
+        return wrapper
+    return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(3))
