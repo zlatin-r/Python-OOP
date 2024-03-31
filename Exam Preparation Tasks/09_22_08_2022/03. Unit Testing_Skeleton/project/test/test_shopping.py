fdc5a17c579c93f8168d2220d60c01c49a60d253
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-03-31 10:49:58 +0300

    Solution to the task

--- Exam Preparation Tasks/09_22_08_2022/03. Unit Testing_Skeleton/project/test/test_shopping.py
+++ Exam Preparation Tasks/09_22_08_2022/03. Unit Testing_Skeleton/project/test/test_shopping.py
@@ -1,1 +1,16 @@
+from unittest import TestCase, main
 from project.shopping_cart import ShoppingCart
+
+
+class TestShopping(TestCase):
+    def setUp(self) -> None:
+        self.cart = ShoppingCart("Metro", 100.00)
+
+    def test_init(self):
+        self.assertEqual("Metro", self.cart.shop_name)
+        self.assertEqual(100.00, self.cart.budget)
+        self.assertEqual({}, self.cart.products)
+
+
+if __name__ == "__main__":
+    main()
