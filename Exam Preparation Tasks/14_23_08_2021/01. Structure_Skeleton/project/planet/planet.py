edca609eaa8278db65b0e4871d9faf9a9da5bc30
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-01 13:25:14 +0300

    Solution to the task

--- Exam Preparation Tasks/14_23_08_2021/01. Structure_Skeleton/project/planet/planet.py
+++ Exam Preparation Tasks/14_23_08_2021/01. Structure_Skeleton/project/planet/planet.py
@@ -5,3 +5,13 @@ class Planet:
     def __init__(self, name: str) -> None:
         self.name = name
         self.items: List = [str]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value.strip():
+            raise ValueError("Planet name cannot be empty string or whitespace!")
+        self.__name = value
