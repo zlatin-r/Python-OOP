f42860b98e3de5fef2149da5fc72a17dfc96a849
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-01 13:05:46 +0300

    Solution to the task

---
+++ Exam Preparation Tasks/14_23_08_2021/01. Structure_Skeleton/project/astronaut/astronaut.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Astronaut(ABC):
+
+    def __init__(self, name: str, oxygen: int):
+        self.name = name
+        self.oxygen = oxygen
+        self.backpack: List = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value.strip():
+            raise ValueError("Astronaut name cannot be empty string or whitespace!")
+        self.__name = value
+
+    @abstractmethod
+    def breathe(self):
+        pass
