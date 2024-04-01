2f1902ffa5dd78f1bec41ceaf723475c88163fd2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-01 09:04:50 +0300

    Solution to the task

--- Exam Preparation Tasks/13_11_12_2021/03. Unit Testing_Skeleton/test/test.py
+++ Exam Preparation Tasks/13_11_12_2021/03. Unit Testing_Skeleton/test/test.py
@@ -1,1 +1,15 @@
+from unittest import TestCase, main
 from project.team import Team
+
+
+class TestTeam(TestCase):
+    def setUp(self):
+        self.team = Team("TestTeam")
+
+    def test_init(self):
+        self.assertEqual(self.team.name, "TestTeam")
+        self.assertEqual(self.team.members, {})
+
+
+if __name__ == '__main__':
+    main()
