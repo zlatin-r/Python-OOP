f441b9d543536d0df94f9b87599f8caa28ad0705
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-01 13:11:19 +0300

    Solution to the task

---
+++ Exam Preparation Tasks/14_23_08_2021/01. Structure_Skeleton/project/astronaut/geodesist.py
@@ -0,0 +1,11 @@
+from project.astronaut.astronaut import Astronaut
+
+
+class Geodesist(Astronaut):
+    INITIAL_OXYGEN = 50
+
+    def __init__(self, name):
+        super().__init__(name, self.INITIAL_OXYGEN)
+
+    def breathe(self):
+        pass
