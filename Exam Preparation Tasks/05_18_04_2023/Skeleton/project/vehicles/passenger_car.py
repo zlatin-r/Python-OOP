0c667c150327f7f821f4dfe73469fa0015955064
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-02 13:47:38 +0300

    Solution to the task

--- Exam Preparation Tasks/05_18_04_2023/Skeleton/project/vehicles/passenger_car.py
+++ Exam Preparation Tasks/05_18_04_2023/Skeleton/project/vehicles/passenger_car.py
@@ -2,9 +2,9 @@ from project.vehicles.base_vehicle import BaseVehicle
 
 
 class PassengerCar(BaseVehicle):
-    MAX_MILEAGE = 450.00
+    MAX_MILEAGE = 450
 
-    def __init__(self, brand: str, model: str, license_plate_number: str):
+    def __init__(self, brand, model, license_plate_number):
         super().__init__(brand, model, license_plate_number, max_mileage=self.MAX_MILEAGE)
 
     def drive(self, mileage: float):
