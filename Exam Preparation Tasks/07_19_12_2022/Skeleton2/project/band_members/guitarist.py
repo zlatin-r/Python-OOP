4381720e51185707f2c0ec55ebd61ec59bcd5e01
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-02 15:39:53 +0300

    Solution to the task

---
+++ Exam Preparation Tasks/07_19_12_2022/Skeleton2/project/band_members/guitarist.py
@@ -0,0 +1,17 @@
+from project.band_members.musician import Musician
+
+
+class Guitarist(Musician):
+    AVAILABLE_SKILLS = ["play metal",
+                        "play rock",
+                        "play jazz"]
+
+    def learn_new_skill(self, new_skill: str):
+        if new_skill not in self.AVAILABLE_SKILLS:
+            raise ValueError(f"{new_skill} is not a needed skill!")
+
+        if new_skill in self.skills:
+            raise ValueError(f"{new_skill} is already learned!")
+
+        self.skills.append(new_skill)
+        return f"{self.name} learned to {new_skill}."
