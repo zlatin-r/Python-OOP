09850ce49368b3433daa560c0ebede2f1c8cc343
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-03 13:56:19 +0300

    Solution to the task

--- Exam Preparation Tasks/10_14_08_2022/02. Structure_Skeleton/project/horse_race_app.py
+++ Exam Preparation Tasks/10_14_08_2022/02. Structure_Skeleton/project/horse_race_app.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from project.horse_race import HorseRace
 from project.horse_specification.appaloosa import Appaloosa
+from project.horse_specification.horse import Horse
 from project.horse_specification.thoroughbred import Thoroughbred
 from project.jockey import Jockey
 
@@ -8,113 +11,74 @@ class HorseRaceApp:
     VALID_HORSE_BREED_TYPES = {"Appaloosa": Appaloosa, "Thoroughbred": Thoroughbred}
 
     def __init__(self):
-        self.horses = []
-        self.jockeys = []
-        self.horse_races = []
+        self.horses: List[Horse] = []
+        self.jockeys: List[Jockey] = []
+        self.horse_races: List[HorseRace] = []
 
     def add_horse(self, horse_type: str, horse_name: str, horse_speed: int):
-        if horse_type in self.VALID_HORSE_BREED_TYPES.keys():
-            if self._find_horse_by_name(horse_name):
-                raise ValueError(f"Horse {horse_name} has been already added!")
+        if horse_type in self.VALID_HORSE_BREED_TYPES:
 
-            new_horse = self.VALID_HORSE_BREED_TYPES[horse_type](horse_name, horse_speed)
-            self.horses.append(new_horse)
-            return f"{horse_type} horse {horse_name} is added."
+            try:
+                next(filter(lambda h: h.name == horse_name, self.horses))
+                raise Exception(f"Horse {horse_name} has been already added!")
+            except StopIteration:
+                self.horses.append(self.VALID_HORSE_BREED_TYPES[horse_type](horse_name, horse_speed))
+                return f"{horse_type} horse {horse_name} is added."
 
     def add_jockey(self, jockey_name: str, age: int):
-        if self._find_jockey_by_name(jockey_name):
-            raise ValueError(f"Jockey {jockey_name} has been already added!")
-
-        new_jockey = Jockey(jockey_name, age)
-        self.jockeys.append(new_jockey)
-        return f"Jockey {jockey_name} is added."
+        try:
+            next(filter(lambda j: j.name == jockey_name, self.jockeys))
+            raise Exception(f"Jockey {jockey_name} has been already added!")
+        except StopIteration:
+            self.jockeys.append(Jockey(jockey_name, age))
+            return f"Jockey {jockey_name} is added."
 
     def create_horse_race(self, race_type: str):
-        if self._check_if_race_type_already_created(race_type):
-            raise ValueError(f"Race {race_type} has been already created!")
-
-        new_race = HorseRace(race_type)
-        self.horse_races.append(new_race)
-        return f"Race {race_type} is created."
+        try:
+            next(filter(lambda r: r.race_type == race_type, self.horse_races))
+            raise Exception(f"Race {race_type} has been already created!")
+        except StopIteration:
+            self.horse_races.append(HorseRace(race_type))
+            return f"Race {race_type} is created."
 
     def add_horse_to_jockey(self, jockey_name: str, horse_type: str):
         jockey = self._find_jockey_by_name(jockey_name)
-        if not jockey:
-            raise Exception(f"Jockey {jockey_name} could not be found!")
+        horse = self._find_horse_by_type(horse_type)
 
-        filtered_horses = [h for h in self.horses if type(h).__name__ == horse_type and not h.is_taken]
-        if not filtered_horses:
-            raise Exception(f"Horse breed {horse_type} could not be found!")
+        if jockey.horse:
+            return f"Jockey {jockey_name} already has a horse"
 
-        if jockey.horse is not None:
-            return f"Jockey {jockey_name} already has a horse."
-
-        horse = filtered_horses.pop()
         jockey.horse = horse
-        horse.is_taken = True
         return f"Jockey {jockey_name} will ride the horse {horse.name}."
 
     def add_jockey_to_horse_race(self, race_type: str, jockey_name: str):
-        race = self._check_if_race_type_is_valid(race_type)
-        if not race:
-            raise Exception(f"Race {race_type} could not be found!")
-
+        race = self._find_race_by_type(race_type)
         jockey = self._find_jockey_by_name(jockey_name)
-        if not jockey:
-            raise Exception(f"Jockey {jockey_name} could not be found!")
 
-        if jockey.horse is None:
+        if not jockey.horse:
             raise Exception(f"Jockey {jockey_name} cannot race without a horse!")
 
-        if self._check_if_jockey_is_added_in_the_race(jockey_name, race):
+        if next(filter(lambda j: j.name == jockey_name, race.jockeys), None):
             return f"Jockey {jockey_name} has been already added to the {race_type} race."
 
         race.jockeys.append(jockey)
         return f"Jockey {jockey_name} added to the {race_type} race."
 
-    def start_horse_race(self, race_type: str):
-        race = self._find_race_by_type(race_type)
+    def _find_race_by_type(self, race_type: str):
+        race = next(filter(lambda r: r.race_type == race_type, self.horse_races), None)
         if not race:
             raise Exception(f"Race {race_type} could not be found!")
+        return race
 
-        if len(self.jockeys) < 2:
-            raise Exception(f"Horse race {race_type} needs at least two participants!")
-
-        best_speed = 0
-        winner_name = ""
-        horse_name = ""
-        for jockey in race.jockeys:
-            speed = jockey.horse.speed
-            if speed > best_speed:
-                winner_name = jockey.name
-                best_speed = speed
-                horse_name = jockey.horse.name
-
-        return (f"The winner of the {race_type} race, "
-                f"with a speed of {best_speed}km/h is "
-                f"{winner_name}! Winner's horse: {horse_name}.")
-
-    # HELPING METHODS
-
-    def _find_horse_by_name(self, horse_name: str):
-        horse = next(filter(lambda h: h.name == horse_name, self.horses), None)
-        return horse
+    def _find_horse_by_type(self, horse_type: str):
+        for i in range(len(self.horses) - 1, -1, -1):
+            if self.horses[i].TYPE_ == horse_type:
+                if not self.horses[i].is_taken:
+                    return self.horses.pop(i)
+        raise Exception(f"Horse breed {horse_type} could not be found!")
 
     def _find_jockey_by_name(self, jockey_name: str):
         jockey = next(filter(lambda j: j.name == jockey_name, self.jockeys), None)
+        if not jockey:
+            raise Exception(f"Jockey {jockey_name} could not be found!")
         return jockey
-
-    def _check_if_race_type_already_created(self, race_type: str):
-        race = next(filter(lambda r: r.race_type == race_type, self.horse_races), None)
-        return race
-
-    def _check_if_race_type_is_valid(self, race_type: str):
-        race = next(filter(lambda r: r.race_type == race_type, self.horse_races), None)
-        return race
-
-    def _check_if_jockey_is_added_in_the_race(self, joke_name: str, race: HorseRace):
-        jockey = next(filter(lambda j: j.name == joke_name, race.jockeys), None)
-        return jockey
-
-    def _find_race_by_type(self, race_type: str):
-        return next(filter(lambda r: r.race_type == race_type, self.horse_races), None)
--- Exam Preparation Tasks/10_14_08_2022/02. Structure_Skeleton/project/horse_specification/appaloosa.py
+++ Exam Preparation Tasks/10_14_08_2022/02. Structure_Skeleton/project/horse_specification/appaloosa.py
@@ -3,6 +3,7 @@ from project.horse_specification.horse import Horse
 
 class Appaloosa(Horse):
     MAX_SPEED = 120
+    TYPE_ = "Appaloosa"
 
     def __init__(self, name, speed):
         super().__init__(name, speed)
