b70fc107ee36a225452a9e484424cba6ec555ade
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-03 12:31:30 +0300

    Solution to the task

--- Exam Preparation Tasks/10_14_08_2022/04. Unit Testing_Skeleton/project/test/test.py
+++ Exam Preparation Tasks/10_14_08_2022/04. Unit Testing_Skeleton/project/test/test.py
@@ -11,75 +11,73 @@ class TestBookstore(TestCase):
         self.assertEqual({}, self.bookstore.availability_in_store_by_book_titles)
         self.assertEqual(0, self.bookstore.total_sold_books)
 
-    def test_setter_books_limit_value_equal_to_zero_raises_value_error(self):
+    def test_books_limit_with_value_less_than_zero_raises_value_error(self):
         with self.assertRaises(ValueError) as ve:
-            self.bookstore.books_limit = 0
+            self.bookstore.books_limit = -1
 
-        self.assertEqual("Books limit of 0 is not valid", str(ve.exception))
+        self.assertEqual("Books limit of -1 is not valid", str(ve.exception))
 
-    def test__len__method(self):
-        self.bookstore.availability_in_store_by_book_titles = {"b1": 2, "b2": 2, "b3": 2, "b4": 2}
+    def test__len__returns_total_books_in_store(self):
+        self.bookstore.availability_in_store_by_book_titles = {"Book1": 2, "Book2": 3, "Book3": 5}
 
-        self.assertEqual(8, len(self.bookstore))
+        self.assertEqual(10, len(self.bookstore))
 
-    def test_receive_book_but_not_enough_space_in_store_raises_exception(self):
-        self.bookstore.books_limit = 2
-        self.bookstore.availability_in_store_by_book_titles = {"b1": 2}
-
+    def test_receive_book_if_not_enough_space_in_store_raise_exception(self):
+        self.bookstore.books_limit = 1
         with self.assertRaises(Exception) as ex:
-            self.bookstore.receive_book("b2", 2)
+            self.bookstore.receive_book("Book", 2)
 
         self.assertEqual("Books limit is reached. Cannot receive more books!", str(ex.exception))
 
-    def test_receive_book_happy_case(self):
+    def test_receive_book_happy_case_book_not_in_store_returns_string_message(self):
+        res = self.bookstore.receive_book("Book", 2)
 
-        self.bookstore.receive_book("b1", 3)
+        self.assertEqual({"Book": 2}, self.bookstore.availability_in_store_by_book_titles)
+        self.assertEqual("2 copies of Book are available in the bookstore.", res)
+        self.assertEqual(2, len(self.bookstore))
 
-        self.assertEqual({"b1": 3}, self.bookstore.availability_in_store_by_book_titles)
-        self.assertEqual(3, len(self.bookstore))
-
-    def test_receive_book_happy_case_returns_correct_string(self):
-        res = self.bookstore.receive_book("b1", 3)
-        self.assertEqual(f"3 copies of b1 are available in the bookstore.", res)
-        self.assertEqual(3, len(self.bookstore))
-
-    def test_sell_book_if_not_available_in_store_raises_exception(self):
-        self.bookstore.availability_in_store_by_book_titles = {"b": 2}
+    def test_sell_book_if_book_is_not_available_in_store_raises_exception(self):
         with self.assertRaises(Exception) as ex:
-            self.bookstore.sell_book("b2", 1)
+            self.bookstore.sell_book("Book", 1)
 
-        self.assertEqual("Book b2 doesn't exist!", str(ex.exception))
+        self.assertEqual("Book Book doesn't exist!", str(ex.exception))
 
-    def test_sell_book_but_not_enough_copies_raises_exception(self):
-        self.bookstore.availability_in_store_by_book_titles = {"b": 1}
+    def test_sell_book_if_not_enough_copies_available_raises_exception(self):
+        self.bookstore.availability_in_store_by_book_titles = {"Book": 1}
 
         with self.assertRaises(Exception) as ex:
-            self.bookstore.sell_book("b", 2)
+            self.bookstore.sell_book("Book", 2)
 
-        self.assertEqual("b has not enough copies to sell. Left: 1", str(ex.exception))
+        self.assertEqual("Book has not enough copies to sell. Left: 1", str(ex.exception))
 
-    def test_sell_book_happy_case(self):
-        self.bookstore.availability_in_store_by_book_titles = {"b": 5}
+    def test_sell_book_happy_case_books_left_returns_string_message(self):
+        self.bookstore.availability_in_store_by_book_titles = {"Book": 5}
 
-        res = self.bookstore.sell_book("b", 2)
+        res = self.bookstore.sell_book("Book", 2)
 
-        self.assertEqual({"b": 3}, self.bookstore.availability_in_store_by_book_titles)
-        self.assertEqual(2, self.bookstore.total_sold_books)
-        self.assertEqual("Sold 2 copies of b", res)
+        self.assertEqual("Sold 2 copies of Book", res)
+        self.assertEqual({"Book": 3}, self.bookstore.availability_in_store_by_book_titles)
         self.assertEqual(3, len(self.bookstore))
 
-    def test__str__without_any_sold_books_and_none_in_stock(self):
-        res = str(self.bookstore)
-        self.assertEqual("Total sold books: 0\nCurrent availability: 0", res)
+    def test_sell_book_happy_case_no_books_left_returns_string_message(self):
+        self.bookstore.availability_in_store_by_book_titles = {"Book": 5}
 
-    def test__str__with_sold_books_and_some_in_stock(self):
-        bookstore = Bookstore(10)
-        bookstore.availability_in_store_by_book_titles = {"a": 5, "b": 3}
-        bookstore.sell_book("a", 3)
+        res = self.bookstore.sell_book("Book", 5)
 
-        res = str(bookstore)
-        self.assertEqual("Total sold books: 3\nCurrent availability: 5\n"
-                         " - a: 2 copies\n - b: 3 copies", res)
+        self.assertEqual("Sold 5 copies of Book", res)
+        self.assertEqual({'Book': 0}, self.bookstore.availability_in_store_by_book_titles)
+        self.assertEqual(5, self.bookstore.total_sold_books)
+        self.assertEqual(0, len(self.bookstore))
+
+    def test__str__(self):
+        self.bookstore.availability_in_store_by_book_titles = {"Book": 4, "Book1": 3}
+        self.bookstore.sell_book("Book", 2)
+
+        result = self.bookstore.__str__()
+        expect = "Total sold books: 2\nCurrent availability: 5\n - Book: 2 copies\n - Book1: 3 copies"
+
+        self.assertEqual(expect, result)
+        self.assertEqual(5, len(self.bookstore))
 
 
 if __name__ == '__main__':
