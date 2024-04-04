f616805a247bf228f3666a92f57f26ff1657a2c8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-04 15:52:46 +0300

    Solution to the task

---
+++ Exam Preparation Tasks/09_22_08_2022/second_try/project/client.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Client:
+    def __init__(self, phone_number: str):
+        self.phone_number = phone_number
+        self.shopping_cart: List[Meal] = []
+        self.bill = 0.0
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        if value[0] != "0" or len(value) != 10 or not value.isdigit():
+            raise ValueError("Invalid phone number!")
+        self.__phone_number = value
