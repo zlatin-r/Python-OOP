b9f1af94a046452612cafe8ec8ca7b0181ea6017
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-13 08:29:14 +0300

    Solution to the task

--- Iterators and Generators - Exercise/More Solutions/01_take_skip.py
+++ Iterators and Generators - Exercise/More Solutions/01_take_skip.py
@@ -2,15 +2,17 @@ class take_skip:
     def __init__(self, step, count):
         self.step = step
         self.count = count
-        self.index = - self.step
+        self.index = 0
+        self.num = -self.step
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        self.num += self.step
         if self.index < self.count:
-            self.index += self.step
-            return self.index
+            self.index += 1
+            return self.num
         raise StopIteration
 
 
