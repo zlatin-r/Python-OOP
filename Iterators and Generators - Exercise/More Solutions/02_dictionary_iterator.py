d2ff0385d2e1c95ead55473190e90b63930af8cd
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-14 09:08:57 +0300

    Solution to the task

--- Iterators and Generators - Exercise/More Solutions/02_dictionary_iterator.py
+++ Iterators and Generators - Exercise/More Solutions/02_dictionary_iterator.py
@@ -1,18 +1,18 @@
 class dictionary_iter:
     def __init__(self, obj: dict):
         self.items = list(obj.items())
-        self.iters = len(obj)
-        self.counter = 0
+        self.index = -1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.counter == self.iters:
+        if self.index >= len(self.items) - 1:
             raise StopIteration
 
-        res = self.items[self.counter]
-        self.counter += 1
+        self.index += 1
+
+        res = self.items[self.index]
 
         return res
 
