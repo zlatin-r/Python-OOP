8bc802f3d40db214c2766fe1a310b6d8f4c71d8f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-15 14:15:04 +0300

    Solution to the task

---
+++ Decorators - Exercise/More Solutions/05_cache.py
@@ -0,0 +1,18 @@
+def cache(func):
+    def wrapper(num):
+        if not wrapper.log.get(num):  # 0(1)
+            wrapper.log[num] = func(num)
+
+        return wrapper.log[num]
+
+    wrapper.log = {}
+
+    return wrapper
+
+
+@cache
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
