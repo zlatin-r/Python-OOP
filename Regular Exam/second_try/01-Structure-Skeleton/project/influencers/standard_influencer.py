6c7c3bfcb1c95879bcfcdbc2b027d28dd45d4e72
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-22 15:17:42 +0300

    Solution to the task

---
+++ Regular Exam/second_try/01-Structure-Skeleton/project/influencers/standard_influencer.py
@@ -0,0 +1,15 @@
+from project.influencers.base_influencer import BaseInfluencer
+
+
+class StandardInfluencer(BaseInfluencer):
+    PAYMENT_PERCENTAGE = 0.45
+
+    def calculate_payment(self, campaign):
+        return campaign.BUDGET * self.PAYMENT_PERCENTAGE
+
+    def reached_followers(self, campaign_type: str):
+        if campaign_type == 'HighBudgetCampaign':
+            return (self.followers * self.engagement_rate) * 1.2
+
+        elif campaign_type == 'LowBudgetCampaign':
+            return (self.followers * self.engagement_rate) * 0.9
