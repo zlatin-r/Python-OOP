84be6d2be21f1461925673f821183d7c3232ff37
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-22 15:10:55 +0300

    Solution to the task

---
+++ Regular Exam/second_try/01-Structure-Skeleton/project/influencers/base_influencer.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+
+class BaseInfluencer(ABC):
+    def __init__(self, username: str, followers: int, engagement_rate: float):
+        self.username = username
+        self.followers = followers
+        self.engagement_rate = engagement_rate
+        self.campaigns_participated = []
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if not value.strip():
+            raise ValueError("Username cannot be empty or consist only of whitespace!")
+        self.__username = value
+        
+    @property
+    def followers(self):
+        return self.__followers
+    
+    @followers.setter
+    def followers(self, value):
+        if value < 0:
+            raise ValueError("Followers must be a non-negative integer!")
+        self.__followers = value
+        
+    @property
+    def engagement_rate(self):
+        return self.__engagement_rate
+    
+    @engagement_rate.setter
+    def engagement_rate(self, value):
+        if not 0 <= value <= 5:
+            raise ValueError("Engagement rate should be between 0 and 5.")
+        self.__engagement_rate = value
+
+    @abstractmethod
+    def calculate_payment(self, campaign):
+        pass
+
+    @abstractmethod
+    def reached_followers(self, campaign_type: str):
+        pass
+
+    def display_campaigns_participated(self):
+        if not self.campaigns_participated:
+            return f"{self.username} has not participated in any campaigns."
+
+        result = [f"{self.username.__class__.__name__} :) {self.username} :) "
+                  f"participated in the following campaigns:"]
+
+        for campaign in self.campaigns_participated:
+                result.append(campaign.reached_followers(campaign.__class__.__name__))
+
+        return "\n".join(result)
