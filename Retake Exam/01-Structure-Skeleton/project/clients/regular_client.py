b92f697bc1977238ab32ee8edc58e52804a5625b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-23 08:42:51 +0300

    Solution to the task

--- Retake Exam/01-Structure-Skeleton/project/clients/regular_client.py
+++ Retake Exam/01-Structure-Skeleton/project/clients/regular_client.py
@@ -4,7 +4,7 @@ from project.clients.base_client import BaseClient
 class RegularClient(BaseClient):
 
     def __init__(self, name):
-        super().__init__(name, "RegularClient")
+        super().__init__(name, "Regular")
 
     def earning_points(self, order_amount: float):
         points_earned = order_amount // 10
